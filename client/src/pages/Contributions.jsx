626d1a74ee6c3b2434f8b3055ebae2ad553f4f1a
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-07 17:21:50 +0530

    console error fixing

--- client/src/pages/Contributions.jsx
+++ client/src/pages/Contributions.jsx
@@ -18,8 +18,8 @@ const Contributions = () => {
             at,
           </p>
 
-          <div class="flex justify-center items-center">
-            <div class="flex">
+          <div className="flex justify-center items-center">
+            <div className="flex">
               <div className="card w-96 bg-base-100 shadow m-5">
                 <div className="card-body">
                   <h2 className="card-title">General Details</h2>
@@ -55,7 +55,7 @@ const Contributions = () => {
                   <form className="">
                     <div className="mt-5">
                       <select className="select select-bordered w-80">
-                        <option disabled selected>
+                        <option defaultValue={"Company Type"}>
                           Company Type
                         </option>
                         <option>Han Solo</option>
@@ -64,7 +64,7 @@ const Contributions = () => {
                     </div>
                     <div className="mt-28">
                       <select className="select select-bordered w-80">
-                        <option disabled selected>
+                        <option defaultValue={"Company Size"}>
                           Company Size
                         </option>
                         <option>Han Solo</option>
@@ -81,7 +81,7 @@ const Contributions = () => {
                   <form className="">
                     <div className="mt-5">
                       <select className="select select-bordered w-80">
-                        <option disabled selected>
+                        <option defaultValue={"Tech"}>
                           Tech
                         </option>
                         <option>Han Solo</option>
@@ -89,7 +89,7 @@ const Contributions = () => {
                       </select>
                     </div>
                   </form>
-                  <div class="flex justify-center items-center">
+                  <div className="flex justify-center items-center">
                     <div className="badge badge-secondary text-center mt-5 mr-2">
                       Node.Js
                     </div>
@@ -100,7 +100,7 @@ const Contributions = () => {
                       React.Js
                     </div>
                   </div>
-                  <div class="flex justify-center items-center">
+                  <div className="flex justify-center items-center">
                     <div className="badge badge-ghost text-center mt-5 mr-1">
                       Laravel
                     </div>
