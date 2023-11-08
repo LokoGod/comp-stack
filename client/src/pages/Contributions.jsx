282dcf55bd51bec3431dea42ce34a63323cc1171
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-11-08 17:08:44 +0530

    latestts

--- client/src/pages/Contributions.jsx
+++ client/src/pages/Contributions.jsx
@@ -18,103 +18,103 @@ const Contributions = () => {
             at,
           </p>
 
-          <div className="flex justify-center items-center">
-            <div className="flex">
-              <div className="card w-96 bg-base-100 shadow m-5">
-                <div className="card-body">
-                  <h2 className="card-title">General Details</h2>
+          <form>
+            <div className="flex justify-center items-center">
+              <div className="flex">
+                <div className="card w-96 bg-base-100 shadow m-5">
+                  <div className="card-body">
+                    <h2 className="card-title">General Details</h2>
 
-                  <form className="">
-                    <div className="mt-5">
-                      <input
-                        type="text"
-                        placeholder="Type here"
-                        className="input input-bordered w-80"
-                      />
-                    </div>
-                    <div className="mt-5">
-                      <textarea
-                        className="textarea textarea-bordered w-80"
-                        placeholder="Bio"
-                      ></textarea>
-                    </div>
-                    <div className="mt-5">
-                      <input
-                        type="text"
-                        placeholder="Type here"
-                        className="input input-bordered w-80"
-                      />
-                    </div>
-                  </form>
-                </div>
-              </div>
-
-              <div className="card w-96 bg-base-100 shadow m-5">
-                <div className="card-body">
-                  <h2 className="card-title">In-Depth Details</h2>
-                  <form className="">
-                    <div className="mt-5">
-                      <select className="select select-bordered w-80">
-                        <option defaultValue={"Company Type"}>
-                          Company Type
-                        </option>
-                        <option>Han Solo</option>
-                        <option>Greedo</option>
-                      </select>
-                    </div>
-                    <div className="mt-28">
-                      <select className="select select-bordered w-80">
-                        <option defaultValue={"Company Size"}>
-                          Company Size
-                        </option>
-                        <option>Han Solo</option>
-                        <option>Greedo</option>
-                      </select>
-                    </div>
-                  </form>
-                </div>
-              </div>
-
-              <div className="card w-96 bg-base-100 shadow m-5">
-                <div className="card-body">
-                  <h2 className="card-title">Technologies Used</h2>
-                  <form className="">
-                    <div className="mt-5">
-                      <select className="select select-bordered w-80">
-                        <option defaultValue={"Tech"}>
-                          Tech
-                        </option>
-                        <option>Han Solo</option>
-                        <option>Greedo</option>
-                      </select>
-                    </div>
-                  </form>
-                  <div className="flex justify-center items-center">
-                    <div className="badge badge-secondary text-center mt-5 mr-2">
-                      Node.Js
-                    </div>
-                    <div className="badge badge-neutral text-center mt-5 mr-2">
-                      .Net
-                    </div>
-                    <div className="badge badge-accent text-center mt-5">
-                      React.Js
-                    </div>
+                    <form className="">
+                      <div className="mt-5">
+                        <input
+                          type="text"
+                          placeholder="Type here"
+                          className="input input-bordered w-80"
+                        />
+                      </div>
+                      <div className="mt-5">
+                        <textarea
+                          className="textarea textarea-bordered w-80"
+                          placeholder="Bio"
+                        ></textarea>
+                      </div>
+                      <div className="mt-5">
+                        <input
+                          type="text"
+                          placeholder="Type here"
+                          className="input input-bordered w-80"
+                        />
+                      </div>
+                    </form>
                   </div>
-                  <div className="flex justify-center items-center">
-                    <div className="badge badge-ghost text-center mt-5 mr-1">
-                      Laravel
+                </div>
+
+                <div className="card w-96 bg-base-100 shadow m-5">
+                  <div className="card-body">
+                    <h2 className="card-title">In-Depth Details</h2>
+                    <form className="">
+                      <div className="mt-5">
+                        <select className="select select-bordered w-80">
+                          <option defaultValue={"Company Type"}>
+                            Company Type
+                          </option>
+                          <option>Han Solo</option>
+                          <option>Greedo</option>
+                        </select>
+                      </div>
+                      <div className="mt-28">
+                        <select className="select select-bordered w-80">
+                          <option defaultValue={"Company Size"}>
+                            Company Size
+                          </option>
+                          <option>Han Solo</option>
+                          <option>Greedo</option>
+                        </select>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+
+                <div className="card w-96 bg-base-100 shadow m-5">
+                  <div className="card-body">
+                    <h2 className="card-title">Technologies Used</h2>
+                    <form className="">
+                      <div className="mt-5">
+                        <select className="select select-bordered w-80">
+                          <option defaultValue={"Tech"}>Tech</option>
+                          <option>Han Solo</option>
+                          <option>Greedo</option>
+                        </select>
+                      </div>
+                    </form>
+                    <div className="flex justify-center items-center">
+                      <div className="badge badge-secondary text-center mt-5 mr-2">
+                        Node.Js
+                      </div>
+                      <div className="badge badge-neutral text-center mt-5 mr-2">
+                        .Net
+                      </div>
+                      <div className="badge badge-accent text-center mt-5">
+                        React.Js
+                      </div>
                     </div>
-                    <div className="badge badge-primary text-center mt-5 mr-1">
-                      Docker
-                    </div>
-                    <div className="badge badge-secondary text-center mt-5">
-                      Terraform
+                    <div className="flex justify-center items-center">
+                      <div className="badge badge-ghost text-center mt-5 mr-1">
+                        Laravel
+                      </div>
+                      <div className="badge badge-primary text-center mt-5 mr-1">
+                        Docker
+                      </div>
+                      <div className="badge badge-secondary text-center mt-5">
+                        Terraform
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
+          </form>
 
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Pitch</button>
